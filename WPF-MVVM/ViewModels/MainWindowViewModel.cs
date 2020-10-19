7cdc0d4fa3687d3adf92705a931650babced6969
NikPikhmanets <nik.pikhmanets@gmail.com>
2020-10-19 19:10:43 +0300

    Test work ViewModel

--- WPF-MVVM/ViewModels/MainWindowViewModel.cs
+++ WPF-MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WPF_MVVM.ViewModels.Base;
 
 namespace WPF_MVVM.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Title window
+
+        private string _Title = "Анализ статистики CV19";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
     }
 }
