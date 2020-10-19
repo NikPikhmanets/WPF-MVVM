2f7af5bf6af5a331b61a9315ff767c54a4f7c34a
NikPikhmanets <nik.pikhmanets@gmail.com>
2020-10-19 13:30:49 +0300

    add console

---
+++ Tests/WPFConcol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPFConcol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
